8105da947303c916b554518d9e8103dd4c88beca
rohan kumar pathak <rohanpathak0171@gmail.com>
2026-09-21 14:46:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (52.54%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,16 +1,41 @@
+                             //optimal approach(Hashmap)      
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         int n = nums.size();
-        int pair = 0;
-
-        for(int i = 0; i<n; i++){
-           for(int j = i+1; j<n;j++){
-            if(nums[i]==nums[j]){
-                pair++;  
-            }
-           } 
+        unordered_map<int , int>freq;
+        
+        for(int & num : nums){
+            freq[num]++;
         }
-        return pair;
+
+        int result = 0;
+
+        for(auto &it : freq){
+            int count = it.second;
+
+            result += (count * (count-1))/2;
+        }
+        return result;
     }
 };
+
+
+
+                             //Brute force
+// class Solution {
+// public:
+//     int numIdenticalPairs(vector<int>& nums) {
+//         int n = nums.size();
+//         int pair = 0;
+
+//         for(int i = 0; i<n; i++){
+//            for(int j = i+1; j<n;j++){
+//             if(nums[i]==nums[j]){
+//                 pair++;  
+//             }
+//            } 
+//         }
+//         return pair;
+//     }
+// };
